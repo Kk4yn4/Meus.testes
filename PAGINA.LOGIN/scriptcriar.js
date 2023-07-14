afbecf5058a31a5669f1a40f447595f32c55ab1f
Kk4yn4 <133814829+Kk4yn4@users.noreply.github.com>
2023-07-13 22:57:25 -0300

    mais att

---
+++ PAGINA.LOGIN/scriptcriar.js
@@ -0,0 +1,17 @@
+function cadastrar(){
+var nome = document.querySelector('input#nometxt').value;
+var sobren = document.querySelector('input#snometxt').value;
+var ema = document.querySelector('input#emailtxt').value;
+var senhan1 = document.querySelector('input#senhatxt').value;
+var senhan2 = document.querySelector('input#confsenhtxt').value;
+
+if(nome = '' || sobren == '' || ema == '' || senhan1 == '' || senhan2 ==''){
+    window.alert('[ERRO] está faltando preencher algum campo!')
+}if(senhan1 == 'um' && senhan2 == 'um'){
+     alert('um')
+}else{
+    alert('A senha difere da primeira')
+} 
+    for()
+
+}
